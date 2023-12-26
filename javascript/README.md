6a61a8ca889bd5194681fc7a5f259c129caf68b2
Official0mega <supr3m30m3ga@gmail.com>
2023-12-26 13:32:53 +0100

    intro to Js

---
+++ javascript/README.md
@@ -0,0 +1,38 @@
+## Introducion to Javascript.
+
+### Variables.
+* let
+* const
+* var
+
+
+## Outputting Variables.
+
+## Data Types
+* Strings
+* Arrays
+* Numbers
+* Boolean
+* Objects
+
+## Type Conversion.
+* parseInt()
+* toString()
+
+## Basic Operations
+* Arithmetic Operations.
+
+## String Operations.
+
+## Conditional Statement
+#### Introduction to Conditionals.
+* if.
+* else if,
+* else.
+
+## Logical Operators
+## Loops(While and For)
+## Looping through Arrays
+## Functions
+## Returning Functions
+## Objects and Methods.
---
+++ javascript/js/script.js
@@ -0,0 +1,53 @@
+// // Primitive Data types
+// let name = "james";
+// let age = 45;
+// let isStudent = true;
+
+// // Arrays
+// let hobbies = ["reading", "coding", "gaming"];
+
+// // Objects
+// let person = {
+//     firstName: "John",
+//     lastName: "Franklyn",
+//     age: 34,
+// };
+
+
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+
+// let numString = "42";
+// let num = parseInt(numString);
+
+// console.log(num);
+
+
+
+// let x = 5;
+// let y = 10; 
+
+// let sum = x + y;
+// let difference = x - y;
+// let product = x * y;
+// let quotient = x / y;
+
+// console.log(sum, difference, product, quotient);
+
+// let greeting = "Hello";
+// let name = "David";
+
+// let welcomeMessage = greeting + ", " + name + "!";
+
+// console.log(welcomeMessage);
+
+let temperature = 45;
+
+if (temperature > 50 ) {
+    console.log("It's a Hot day!");
+} else if (temperature > 32 ) {
+    console.log("It's a normal day and weather is okay.");
+} else {
+    console.log("It's a cold day.!")
+}
