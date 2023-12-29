1b5b1f48c3f7198d5d411b1f76c012a2f398d4ec
Official0mega <supr3m30m3ga@gmail.com>
2023-12-29 21:36:03 +0100

    for today DOM

---
+++ javascript/Today_29_12_2023/js/script.js
@@ -0,0 +1,15 @@
+// // Accessing an element by ID
+// let titleElement = document.getElementById("mainTitle");
+
+// // Modifying the content
+// titleElement.innerHTML = "Hello, Dom";
+
+
+
+// Accessing the Element by ID
+let titleElement = document.getElementById("mainTitle");
+
+// Modifying the styles dynamically.
+titleElement.style.color = "blue";
+titleElement.style.fontSize = "40px";
+titleElement.style.background = "red";
