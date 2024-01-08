6ee12d15ddba0fd9090c8d4b1c4147910500294e
Official0mega <supr3m30m3ga@gmail.com>
2024-01-08 17:06:21 +0100

    hamburger button added

--- HTML_CSS_JS/js/script.js
+++ HTML_CSS_JS/js/script.js
@@ -41,3 +41,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 })
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navLinks = document.querySelector('nav ul');
+
+    menuIcon.addEventListener('click', () => {
+        navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+    });
+});
