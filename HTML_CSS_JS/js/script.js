82301e96e514fbf0e9bd15988ff53b931d042344
Official0mega <supr3m30m3ga@gmail.com>
2024-01-08 16:01:21 +0100

    updated css and js

---
+++ HTML_CSS_JS/js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Animation for the welcome Texts
+    const welcomeTexts = document.querySelectorAll('#home p, #home h2');
+
+    welcomeTexts.forEach(text => {
+        text.style.opacity = '0';
+    });
+
+    setTimeout(() => {
+        welcomeTexts.forEach(text => {
+            text.style.opacity = '1';
+            text.style.transition = 'opacity 1s ease-in-out';
+        });
+    }, 300);
+});
+// Smooth Scrolling for Navigation Links
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop - 50, 
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const backToTopButton = document.getElementById('back-to-top-btn');
+
+    window.addEventListener('scroll', () => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+})
